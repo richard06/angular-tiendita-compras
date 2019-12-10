7c61476c769eb837714fc7195ce66dcef8dc2945
richard06 <richard88gm@gmail.com>
2019-12-09 21:24:49 -0600

    Se agrego el componente para el Formulario de pago del carrito

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 import { CartService } from '../cart.service';
 
@@ -9,13 +10,31 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items;
+  checkoutForm;
 
   constructor(
-    private cartService: CartService
-  ) { }
+    //inyeccion de dependencias
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+  ) { 
+    this.items=this.cartService.getItems();
+    this.checkoutForm= this.formBuilder.group({
+      name: '',
+      address: ''
+    })
+  }
 
   ngOnInit() {
     this.items=this.cartService.getItems();//obtener todos los productos seleccionados para comprar
+  }
+
+//Metodo para que los usuarios puedan enviar sus datos a trvaez del formulario, despues de ello, el formulario debe reiniciarse y el carrito borarse
+  onSubmit(customerData){
+    //Proceso de datos de pago aqui
+    console.warn('SU pedido ha sido enviado', customerData);
+
+    this.items= this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
 }
